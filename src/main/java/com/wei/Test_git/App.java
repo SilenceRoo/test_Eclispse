08dbb9ee086997b55ef09a06bdb8a52346e02f46
Silence <1371096662@qq.com>
2020-03-12 18:30:55 +0800

    my fitst git in eclipse

---
+++ src/main/java/com/wei/Test_git/App.java
@@ -0,0 +1,13 @@
+package com.wei.Test_git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/wei/Test_git/zzzz.java
@@ -0,0 +1,11 @@
+package com.wei.Test_git;
+
+public class zzzz {
+
+	public static void main(String[] args) {
+		String string = new String();
+		Object str = "a";
+		boolean equals = string.equals(str);
+		System.out.println(equals);
+	}
+}
